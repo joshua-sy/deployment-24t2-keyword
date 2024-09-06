c8ff2b8af831e2c0471a04fce5effc952c418dc6
David Hua <z5529749@ad.unsw.edu.au>
2024-09-06 20:24:20 +1000

    Moved crossIcon.png to icons

--- components/keyword/rules/Rules.tsx
+++ components/keyword/rules/Rules.tsx
@@ -20,7 +20,7 @@ const Rules: React.FC<RulesProps> = ({
 }) => {
   return (
     <div className="relative">
-      <img src="/crossIcon.png" alt="Cross Icon" onClick={onClose}
+      <img src="./icons/crossIcon.png" alt="Cross Icon" onClick={onClose}
         className="w-10 h-10 absolute top-2 right-2 cursor-pointer" />
       <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white text-center p-4">{title}</h2>
       <p className="text-base sm:text-lg md:text-xl text-white mt-0 font-bold px-4 sm:px-6" dangerouslySetInnerHTML={{ __html: parseText(content) }} />
