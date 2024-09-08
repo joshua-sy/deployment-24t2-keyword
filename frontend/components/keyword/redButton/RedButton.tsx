92236cf9847c429ddb7dc5422d75195fcff8ef20
Joshua Sy <z5259375@unsw.edu.au>
2024-09-09 01:54:38 +1000

    added sound effects and hover colour to redButton

--- frontend/components/keyword/redButton/RedButton.tsx
+++ frontend/components/keyword/redButton/RedButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 interface redButtonProps {
   label: string;
@@ -9,12 +9,33 @@ const RedButton: React.FC<redButtonProps> = ({
   label,
   onClick,
 }) => {
+  const clickSoundRef = useRef<HTMLAudioElement | null>(null);
+  const hoverSoundRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    clickSoundRef.current = new Audio('/soundEffects/redButtonClickSoundEffect.mp3');
+    hoverSoundRef.current = new Audio('/soundEffects/redButtonHoverSoundEffect.mp3');
+  }, []);
+
+  const handleHover = () => {
+    hoverSoundRef.current?.play();
+  }
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    clickSoundRef.current?.play();
+    if (onClick) {
+      onClick();
+    }
+  };
+
+
   return (
     <>
       <div className='buttonDiv py-3'>
         <button
-          className={"bg-figma-red border-black border-2 text-white text-2xl font-bold py-2 px-4 rounded w-[400px]"}
-          onClick={onClick}
+          className={"bg-figma-red border-black border-2 text-white text-2xl font-bold py-2 px-4 rounded w-[400px] hover:bg-red-600"}
+          onClick={handleClick}
+          onMouseEnter={handleHover}
         >
           {label}
         </button>
